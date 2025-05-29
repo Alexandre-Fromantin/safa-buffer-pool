4036aecbdd16e41f91ed1ea32fb9de99a6df7ea1
Alexandre-Fromantin <oalexandrefromantin@gmail.com>
2025-05-29 09:33:58 +0200

    start documenting

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-A tool, to create and manage buffer pools quickly and easily in Rust
+Safa-buffer-pool is a quick and simple tool to create buffer pools in a mono or multi thread context.
+
+# Overview
+
+To create a pool you must use BufferPoolBuilder, define or not these parameters and used the build_mono_thread or build_multi_thread functions according to the context.
+
+
+### Lifetime
+
+When the number of buffers is greater than the minimum defined, they are removed according to the life of the over buffer(the over buffersare that are not used between interval of the life).  
+A(first call) --> lifetime --> B(end call): All buffers not used between moments A and B are drop (keeps the minimum buffer).  
+⚠️In Mono-Thread context, the lifetime does not work automatically, you must use reduce_allocated_buffer() which will calculate the buffer overages.
+
+# Exemple
+
+### Mono-Thread
--- src/lib.rs
+++ src/lib.rs
@@ -82,35 +82,35 @@ mod tests {
 
         let pool_cl = pool.clone();
         let a = tokio::spawn(async move {
-            for _ in 0..100000 {
-                //println!("A: try to get a new buffer");
+            for _ in 0..10000 {
+                println!("A: try to get a new buffer");
                 let new_buffer_opt = pool_cl.get().await;
                 if new_buffer_opt.is_none() {
-                    //println!("A: buffer no available");
+                    println!("A: buffer no available");
                     break;
                 }
                 let mut new_buffer = new_buffer_opt.unwrap();
                 new_buffer[0] = 0x01;
                 new_buffer[1] = 0x02;
                 new_buffer[2] = 0x03;
-                //println!("A: free a buffer");
+                println!("A: free a buffer");
             }
         });
 
         let pool_cl2 = pool.clone();
         let b = tokio::spawn(async move {
-            for _ in 0..100000 {
-                //println!("B: try to get a new buffer");
+            for _ in 0..10000 {
+                println!("B: try to get a new buffer");
                 let new_buffer_opt = pool_cl2.get().await;
                 if new_buffer_opt.is_none() {
-                    //println!("B: buffer no available");
+                    println!("B: buffer no available");
                     break;
                 }
                 let mut new_buffer = new_buffer_opt.unwrap();
                 new_buffer[0] = 0x01;
                 new_buffer[1] = 0x02;
                 new_buffer[2] = 0x03;
-                //println!("B: free a buffer");
+                println!("B: free a buffer");
             }
         });
 
@@ -133,27 +133,27 @@ mod tests {
             .set_buffer_size(1024 * 1024)
             .set_min_number_of_buffer(100)
             .set_max_number_of_buffer(200)
-            .set_over_buffer_lifetime(Duration::from_secs(10))
+            .set_over_buffer_lifetime(Duration::from_secs(2))
             .build_multi_thread();
 
         for _ in 0..150 {
             let pool_cl = pool.clone();
             task::spawn(async move {
                 let _buffer = pool_cl.get().await;
-                sleep(Duration::from_secs(11)).await;
+                sleep(Duration::from_secs(3)).await;
             });
         }
 
-        sleep(Duration::from_secs(25)).await;
+        sleep(Duration::from_secs(4)).await;
 
         for _ in 0..120 {
             let pool_cl = pool.clone();
             task::spawn(async move {
                 let _buffer = pool_cl.get().await;
-                sleep(Duration::from_secs(15)).await;
+                sleep(Duration::from_secs(3)).await;
             });
         }
 
-        sleep(Duration::from_secs(25)).await;
+        sleep(Duration::from_secs(5)).await;
     }
 }
